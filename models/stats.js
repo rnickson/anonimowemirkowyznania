2a642c8e97ed40118086e8543e3cc10fb84f65ca
filip <fiqxio@gmail.com>
2017-10-11 23:44:28 +0200

    small date fixes in stats addAction statis method

--- models/stats.js
+++ models/stats.js
@@ -7,8 +7,8 @@ const statsModel = new mongoose.Schema({
 });
 statsModel.statics.addAction = function(type, username=undefined){
 var date = new Date();
-var periods = [['year', `${date.getFullYear()}`], ['month', `${date.getMonth()}-${date.getFullYear()}`], [
-    'day', `${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`]];
+var periods = [['year', `${date.getFullYear()}`], ['month', `${date.getMonth()+1}-${date.getFullYear()}`], [
+    'day', `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`]];
 for (var i = 0; i < periods.length; i++) {
     var increment = {};
     increment[`count.${type}.${periods[i][1]}`] = 1;
