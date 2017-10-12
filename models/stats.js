aa393c72acffc12d034735c340de35d64036875d
filip <fiqxio@gmail.com>
2017-10-12 20:09:08 +0200

    modified a little schema model

--- models/stats.js
+++ models/stats.js
@@ -3,17 +3,19 @@ var Schema = mongoose.Schema;
 
 const statsModel = new mongoose.Schema({
     period: {type: mongoose.Schema.Types.String, enum:['day', 'month', 'year']},
-    count: mongoose.Schema.Types.Mixed
+    date: Date,
+    count: mongoose.Schema.Types.Mixed,
+    users: mongoose.Schema.Types.Mixed
 });
 statsModel.statics.addAction = function(type, username=undefined){
 var date = new Date();
-var periods = [['year', `${date.getFullYear()}`], ['month', `${date.getMonth()+1}-${date.getFullYear()}`], [
-    'day', `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`]];
+var periods = [['year', new Date(date.getFullYear(), 0, 1), `${date.getFullYear()}`], ['month', new Date(date.getFullYear(), date.getMonth()), `${date.getMonth()+1}-${date.getFullYear()}`], [
+    'day', new Date(date.getFullYear(), date.getMonth(), date.getDate()), `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`]];
 for (var i = 0; i < periods.length; i++) {
     var increment = {};
-    increment[`count.${type}.${periods[i][1]}`] = 1;
-    if(username)increment[`count.users.${username}.${type}.${periods[i][1]}`] = 1;
-    this.updateOne({period: periods[i][0]}, {period: periods[i][0], $inc:increment}, {upsert: true}).exec(err=>{
+    increment[`count.${type}.${periods[i][2]}`] = 1;
+    if(username)increment[`users.${username}.${type}.${periods[i][2]}`] = 1;
+    this.updateOne({date: periods[i][1], period: periods[i][0]}, {date: periods[i][1], period: periods[i][0], $inc:increment}, {upsert: true}).exec(err=>{
     if(err)console.log(err);
     });
 }
